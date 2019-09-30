8c775c1ce4eb772c0dd131c7f9e2162846ecb9a9
Aleksey Shardin <aleksey.shardin@mail.ru>
2019-09-30 13:25:10 +0500

    Add input function

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,20 +3,24 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.awt.print.Book;
+
 public class Bot extends TelegramLongPollingBot {
+
+    Book book = new Book();
+    private long chat_id;
+
     @Override
     public void onUpdateReceived(Update update) {
         update.getUpdateId();
 
         SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
-
-        if (update.getMessage().getText().equals("Привет")) {
-            sendMessage.setText("Hello, my friend!");
-            try {
-                execute(sendMessage);
-            }catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+        chat_id = update.getMessage().getChatId();
+        sendMessage.setText(input(update.getMessage().getText()));
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
     @Override
@@ -26,5 +30,12 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "884804841:AAFL7evBxdUw-RvWM-7G9In6GbB_jp4M7Vk";
+    }
+
+    public String input(String msg){
+        if(msg.contains("Hello") || msg.contains("Hi") || msg.contains("Привет")){
+            return "Hello, my friend!";
+        }
+        return msg;
     }
 }
