f7af151626dddcbdb7db347cddf0bb5b403a0fd4
Aleksey Shardin <aleksey.shardin@mail.ru>
2019-10-02 13:51:18 +0500

    Add replykeyboard

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,9 +1,14 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -13,9 +18,8 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         update.getUpdateId();
-
-        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
         chat_id = update.getMessage().getChatId();
+        SendMessage sendMessage = new SendMessage().setChatId(chat_id);
         sendMessage.setText(input(update.getMessage().getText()));
         try {
             execute(sendMessage);
@@ -32,10 +36,38 @@ public class Bot extends TelegramLongPollingBot {
         return "884804841:AAFL7evBxdUw-RvWM-7G9In6GbB_jp4M7Vk";
     }
 
-    public String input(String msg){
+    public String input(String msg) {
         if(msg.contains("Hello") || msg.contains("Hi") || msg.contains("Привет")){
             return "Hello, my friend!";
         }
         return msg;
+    }
+
+    public synchronized void setButtons(SendMessage sendMessage) {
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add(new KeyboardButton("Привет"));
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add(new KeyboardButton("Помощь"));
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,10 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
     public static void main (String[] args) throws TelegramApiRequestException {
+        System.getProperties().put("proxySet", "true");
+        System.getProperties().put("socksProxyHost", "127.0.0.1");
+        System.getProperties().put("socksProxyPort", "9150");
+
         ApiContextInitializer.init();
         TelegramBotsApi telegram = new TelegramBotsApi();
 
