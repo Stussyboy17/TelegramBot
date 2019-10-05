e9fb695ce7b755facc770dee5de34f9defd32b6d
Aleksey Shardin <aleksey.shardin@mail.ru>
2019-10-05 22:04:01 +0500

    minor fixes and updates

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,21 +6,20 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
 
-    Book book = new Book();
     private long chat_id;
+    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
     @Override
     public void onUpdateReceived(Update update) {
         update.getUpdateId();
         chat_id = update.getMessage().getChatId();
         SendMessage sendMessage = new SendMessage().setChatId(chat_id);
-        setButtons(sendMessage);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
         sendMessage.setText(input(update.getMessage().getText()));
         try {
             execute(sendMessage);
@@ -38,40 +37,32 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public String input(String msg) {
-        if( msg.toLowerCase().contains("hello") || msg.toLowerCase().contains("hi") || msg.toLowerCase().contains("привет")){
-            return "Приветсвую тебя, другой мой";
-        }
-        if (msg.contains("Каво?")){
-            return "Это простой бот который пока что может дублировать Ваши сообщения, остальное будет позже";
-        }
-        if (msg.contains("[В разработке]")){
-            return "Этот раздел пока в разработке, попробуйте что-нибудь другое";
-        }
-        return msg;
-    }
-
-    public synchronized void setButtons(SendMessage sendMessage) {
-        // Создаем клавиуатуру
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
         replyKeyboardMarkup.setOneTimeKeyboard(false);
 
         List<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
-
         KeyboardRow keyboardFirstRow = new KeyboardRow();
-
-        keyboardFirstRow.add(new KeyboardButton("Привет"));
-        keyboardFirstRow.add(new KeyboardButton("[В разработке]"));
-
         KeyboardRow keyboardSecondRow = new KeyboardRow();
 
-        keyboardSecondRow.add(new KeyboardButton("Каво?"));
-
-        keyboard.add(keyboardFirstRow);
-        keyboard.add(keyboardSecondRow);
-
-        replyKeyboardMarkup.setKeyboard(keyboard);
+        if( msg.toLowerCase().contains("hello") || msg.toLowerCase().contains("hi") || msg.toLowerCase().contains("привет")){
+            keyboardFirstRow.add(new KeyboardButton("Как дела у бота?"));
+            keyboardFirstRow.add(new KeyboardButton("[В разработке]"));
+            keyboardSecondRow.add(new KeyboardButton("Каво?"));
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Приветствую тебя, друг мой, чего желаешь?";
+        }
+        if (msg.contains("Каво?")){
+            return "Это простой бот, который пока что может дублировать Ваши сообщения, остальное будет позже";
+        }
+        if (msg.contains("[В разработке]")) {
+            return "Этот раздел пока в разработке, попробуйте что-нибудь другое";
+        }
+        if (msg.contains("Проверить делишки бота")) {
+            return "У меня все хорошо, спасибо, что интересуетесь♥";
+        }
+        return msg;
     }
 }
